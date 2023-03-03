e7ee59312c2f5138522cfb8a946d8fcf5bee2b74
Yue Zheng Ting <yuezhengting@gmail.com>
2023-03-04 00:53:06 +0800

    Add SQL Solution for Question 177

---
+++ sql/177_nth_highest_salary.sql
@@ -0,0 +1,12 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  SET n = N - 1;
+  RETURN (
+      # Write your MySQL query statement below.
+      SELECT distinct(e.salary) 
+      FROM employee as e
+      ORDER BY e.salary DESC
+      LIMIT 1
+      OFFSET n
+  );
+END
